3e057bdbb2048caee838923c065abef39c18d319
heikoschmidt1187 <github@heikoschmidt.info>
2020-06-22 20:41:46 +0200

    working

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,17 +1,17 @@
 export const config = {
   "dev": {
-    "username": "udagramhscdev",
-    "password": "",
-    "database": "udagramhscdev",
-    "host": "udagramhscdev.ccyvta7poonl.eu-central-1.rds.amazonaws.com",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "eu-central-1a",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-hsc-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "udagramhscdev",
-    "password": "",
+    "password": "passwordhere",
     "database": "udagram_prod",
     "host": "udagramhscdev.ccyvta7poonl.eu-central-1.rds.amazonaws.com",
     "dialect": "postgres"
